da7d42e0f6bb4c9aff5d2cc70b30dfe465ce9829
Manali2311 <manalisharma2311@gmail.com>
2020-05-03 11:45:56 +0530

    Updates

---
+++ precache-manifest.11f09edc485d59b28265be3f25ff6541.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16e43b6bd62fdccba6f50f9eed43ae2b",
+    "url": "/office-life/index.html"
+  },
+  {
+    "revision": "9541398b3ad32bfb9307",
+    "url": "/office-life/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "24fc3d95a2f31badf9ed",
+    "url": "/office-life/static/css/main.cbb4f630.chunk.css"
+  },
+  {
+    "revision": "9541398b3ad32bfb9307",
+    "url": "/office-life/static/js/2.ef3f2a71.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/office-life/static/js/2.ef3f2a71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24fc3d95a2f31badf9ed",
+    "url": "/office-life/static/js/main.179d0d20.chunk.js"
+  },
+  {
+    "revision": "59db29f263b3158938a5",
+    "url": "/office-life/static/js/runtime-main.8f9af8de.js"
+  }
+]);
